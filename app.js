6c1ea22532a201255010f529800085d07c79cd27
ntdtiendat712 <ntdtiendat712@gmail.com>
2023-09-07 22:43:50 +0700

    Open new tab on brower after starting nodejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hbs = require("express-hbs");
 const express = require("express");
 const app = express();
 const port = 3000;
+const LOCAL_HOST = `http://localhost:${port}`;
 
 const viewsDir = "./views";
 
@@ -21,5 +22,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`App listening on port ${LOCAL_HOST}`);
+  require("child_process").exec(`start ${LOCAL_HOST}`);
 });
