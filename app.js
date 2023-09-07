f83276e0c87f8280a9e6efdc1aca25f1aecb5423
ntdtiendat712 <ntdtiendat712@gmail.com>
2023-09-07 22:29:08 +0700

    add node js

---
+++ app.js
@@ -0,0 +1,25 @@
+const hbs = require("express-hbs");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const viewsDir = "./views";
+
+app.engine(
+  "hbs",
+  hbs.express4({
+    partialsDir: viewsDir,
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", viewsDir);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Stack of work",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
